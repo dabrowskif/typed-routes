148d086128089b62ec7c28c90d897433ace17de7
Filip Dabrowski <filip.dabrowski@protonmail.com>
2024-08-25 12:06:52 +0200

    chore: add eslint and prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,56 @@
+import eslint from "@eslint/js";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import unusedImports from "eslint-plugin-unused-imports";
+import tseslint from "typescript-eslint";
+
+const ignoreFilesConfig = {
+  ignores: [
+    "dist",
+    "node_modules",
+    "coverage",
+    "README.md",
+    "CHANGELOG.md",
+    "LICENSE",
+  ],
+};
+
+const autoImportSortPlugin = {
+  plugins: {
+    "simple-import-sort": simpleImportSort,
+  },
+  rules: {
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+  },
+};
+
+const removeUnusedImportsPlugin = {
+  plugins: {
+    "unused-imports": unusedImports,
+  },
+  rules: {
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-vars": [
+      "error",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_",
+      },
+    ],
+  },
+};
+
+export default tseslint.config(
+  ignoreFilesConfig,
+  eslint.configs.recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
+  eslintPluginPrettierRecommended,
+  autoImportSortPlugin,
+  removeUnusedImportsPlugin,
+);
